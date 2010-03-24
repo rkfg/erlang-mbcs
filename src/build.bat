34f5717c27155828cbca9a7a910d1188b0f53ba6
luxiangyu@msn.com <luxiangyu@msn.com@f126f710-1157-11df-b706-7162ac63f06b>
2010-03-24 17:27:12 +0000

    del build.bat

--- src/build.bat
+++
@@ -1,8 +0,0 @@
-@path D:\erl5.7.5\bin;%Path%
-@del /f /q /s ..\ebin\*.beam ..\ebin\*.app .\erl_crash.dump >NUL
-@for %%I in (*.erl) do (
-    erlc +debug_info -o ../ebin %%I)
-)
-copy /y mbcs.app ..\ebin
-erl -noshell -s mbcs_test test -s init stop
-pause
